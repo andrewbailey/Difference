124dd4b2882177f42b28e89e101a23d02b739a3c
Andrew Bailey <me@andrewbailey.dev>
2020-05-25 01:34:32 -0400

    Fix ktlint errors

--- diff-perf-test/src/androidTest/java/dev/andrewbailey/diff/DiffBenchmarkTest.kt
+++ diff-perf-test/src/androidTest/java/dev/andrewbailey/diff/DiffBenchmarkTest.kt
@@ -1,14 +1,12 @@
 package dev.andrewbailey.diff
 
-import android.Manifest
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.benchmark.junit4.measureRepeated
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.GrantPermissionRule
+import kotlin.random.Random
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.random.Random
 
 @RunWith(AndroidJUnit4::class)
 class DiffBenchmarkTest {
