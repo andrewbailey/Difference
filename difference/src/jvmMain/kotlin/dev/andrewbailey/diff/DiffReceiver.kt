bb7f712da5399d3d27c2128fe2fff025c79f1531
Andrew Bailey <me@andrewbailey.dev>
2020-06-28 13:31:24 -0400

    Improve Java interop

---
+++ difference/src/jvmMain/kotlin/dev/andrewbailey/diff/DiffReceiver.kt
@@ -0,0 +1,78 @@
+package dev.andrewbailey.diff
+
+import dev.andrewbailey.diff.DiffOperation.*
+
+/**
+ * This class serves as a convenience class for Java users who may find it tedious to call [DiffResult.applyDiff],
+ * since Java users have to rely on Kotlin's `FunctionN` interfaces and don't have access to Kotlin's named arguments
+ * and default arguments.
+ *
+ * If you're using Difference directly in Kotlin, then there's no reason for you to use this class since the
+ * [DiffResult.applyDiff] is a more idiomatic way to use a difference result.
+ */
+abstract class DiffReceiver<T> {
+
+    fun applyDiff(diff: DiffResult<T>) {
+        diff.operations.forEach { operation ->
+            when (operation) {
+                is Remove -> {
+                    remove(operation.index)
+                }
+                is RemoveRange -> {
+                    removeRange(operation.startIndex, operation.endIndex)
+                }
+                is Add -> {
+                    insert(operation.item, operation.index)
+                }
+                is AddAll -> {
+                    insertAll(operation.items, operation.index)
+                }
+                is Move -> {
+                    move(operation.fromIndex, operation.toIndex)
+                }
+                is MoveRange -> {
+                    moveRange(operation.fromIndex, operation.toIndex, operation.itemCount)
+                }
+            }
+        }
+    }
+
+    abstract fun remove(index: Int)
+
+    open fun removeRange(start: Int, end: Int) {
+        repeat(times = end - start) {
+            remove(start)
+        }
+    }
+
+    abstract fun insert(item: T, index: Int)
+
+    open fun insertAll(items: List<T>, index: Int) {
+        items.forEachIndexed { itemIndex, item ->
+            insert(item, index + itemIndex)
+        }
+    }
+
+    open fun move(oldIndex: Int, newIndex: Int) {
+        throw UnsupportedOperationException("The received diff included move operations, but " +
+                "this receiver does not support moving elements. You should either disable " +
+                "movement detection when generating the diff, or override the " +
+                "`DiffReceiver.move()` function.")
+    }
+
+    open fun moveRange(oldIndex: Int, newIndex: Int, count: Int) {
+        when {
+            newIndex < oldIndex -> {
+                (0 until count).forEach { item ->
+                    move(oldIndex + item, newIndex + item)
+                }
+            }
+            newIndex > oldIndex -> {
+                repeat(count) {
+                    move(oldIndex, newIndex)
+                }
+            }
+        }
+    }
+
+}
---
+++ difference/src/jvmTest/kotlin/dev/andrewbailey/diff/DiffReceiverTest.kt
@@ -0,0 +1,85 @@
+package dev.andrewbailey.diff
+
+import kotlin.random.Random
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class DiffReceiverTest {
+
+    @Test
+    fun `applyDiff on original value returns updated value`() {
+        val original = generateList()
+        val updated = generateModifiedList(original)
+
+        val diff = differenceOf(original, updated, false)
+
+        val appliedResult = original.toMutableList()
+
+        object : DiffReceiver<String>() {
+            override fun remove(index: Int) {
+                appliedResult.removeAt(index)
+            }
+
+            override fun insert(item: String, index: Int) {
+                appliedResult.add(index, item)
+            }
+
+            override fun move(oldIndex: Int, newIndex: Int) {
+                appliedResult.add(
+                    element = appliedResult.removeAt(oldIndex),
+                    index = if (newIndex < oldIndex) {
+                        newIndex
+                    } else {
+                        newIndex - 1
+                    }
+                )
+            }
+        }.applyDiff(diff)
+
+        assertEquals(
+            message = "Applying the diff to the input did not yield the updated value.",
+            expected = updated,
+            actual = appliedResult
+        )
+    }
+
+    private fun generateList(): List<String> {
+        val random = Random(319163183995026179)
+        return List(500) { random.nextInt().toString() }
+    }
+
+    private fun generateModifiedList(
+        originalData: List<String>
+    ): List<String> {
+        val random = Random(3260128955430943624)
+        val modifiedList = originalData.toMutableList()
+
+        repeat(250) {
+            when (random.nextInt(0, 3)) {
+                0 -> { // Delete
+                    modifiedList.removeAt(
+                        index = random.nextInt(0, modifiedList.size)
+                    )
+                }
+                1 -> { // Insert
+                    modifiedList.add(
+                        index = random.nextInt(0, modifiedList.size + 1),
+                        element = random.nextInt().toString()
+                    )
+                }
+                2 -> { // Move
+                    val item = modifiedList.removeAt(
+                        index = random.nextInt(0, modifiedList.size)
+                    )
+                    modifiedList.add(
+                        index = random.nextInt(0, modifiedList.size + 1),
+                        element = item
+                    )
+                }
+            }
+        }
+
+        return modifiedList
+    }
+
+}
