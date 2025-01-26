cf45d8e8ca2062b45c67a0128ad68f567e03c065
Andrew Bailey <me@andrewbailey.dev>
2025-01-26 17:06:07 -0500

    Reduce number of allocations

--- difference/src/commonMain/kotlin/dev/andrewbailey/diff/impl/Extensions.kt
+++ difference/src/commonMain/kotlin/dev/andrewbailey/diff/impl/Extensions.kt
@@ -2,17 +2,26 @@ package dev.andrewbailey.diff.impl
 
 import kotlin.math.abs
 
-internal fun Int.isEven() = abs(this) % 2 == 0
+internal inline fun Int.isEven() = abs(this) % 2 == 0
 
-internal fun Int.isOdd() = abs(this) % 2 == 1
+internal inline fun Int.isOdd() = abs(this) % 2 == 1
 
-internal fun <T> MutableList<T>.push(item: T) {
+internal inline fun <T> MutableList<T>.push(item: T) {
     add(item)
 }
 
-internal fun <T> MutableList<T>.pop(): T {
-    check(isNotEmpty()) {
-        "List has no items"
+internal inline fun <T> MutableList<T>.pop(): T = removeAt(size - 1)
+
+internal inline fun <T> List<T>.fastForEach(action: (T) -> Unit) {
+    @Suppress("ReplaceManualRangeWithIndicesCalls")
+    for (i in 0 until size) {
+        action(this[i])
     }
-    return removeAt(size - 1)
+}
+
+internal inline fun <T> List<T>.fastForEachIndexed(action: (Int, T) -> Unit) {
+    @Suppress("ReplaceManualRangeWithIndicesCalls")
+    for (i in 0 until size) {
+        action(i, this[i])
+    }
 }
